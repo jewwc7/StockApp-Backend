49322f0ba0cc9ea7d2fe5e374ddc9c1b7b85efd7
jewwc7 <67772782+jewwc7@users.noreply.github.com>
2022-04-07 19:09:41 -0500

    aws firstUpload

--- backend/routes/asyncFunctions.js
+++ backend/routes/asyncFunctions.js
@@ -1,23 +1,11 @@
-const {
-  deleteFriend,
-  updateUserById,
-  findUser,
-  getCustomers,
-  updateUser,
-  findUserById,
-  findCustomerAndUpdate,
-  addUserToDbAppUsers,
-  updateUserArr,
-  updateCommunityArr,
-  addUsertoCommunityArr,
-  findData,
-  deleteFromUserArr,
-  deleteCollectionDocs,
-  getPriceFromDb,
-} = require("./gainsMongoFunctions");
-const { getQuote, getIntraday } = require("./apiFunctions");
+const axios = require("axios");
+const { getPriceFromDb } = require("./gainsMongoFunctions");
+const { getQuote, getIntraday, getDaily } = require("./apiFunctions");
+const { myFunctions } = require("./backendMyFunctions");
 const { isBefore } = require("date-fns");
+const { getDatePrices } = myFunctions;
 
+///used when fund first created
 async function appendTodaysPrice(arr) {
   const collection = "stock prices";
   const completeTickers = Promise.all(
@@ -41,22 +29,29 @@ async function appendTodaysPrice(arr) {
       let tickerPrice = parseFloat(quote["Global Quote"]["05. price"]).toFixed(
         2
       );
+      const tickerPriceObj = {
+        value: tickerPrice,
+        timestamp: new Date(),
+      };
       return {
         //if quore not in db, make api call and reutrn the quote object
         ...element, //return the orginal element with the purchase price added. Will be the and stay the orginal investment of the fund
         purchasePrice: tickerPrice,
         currentPrice: tickerPrice, //will be updated each day
         amountPurchased: percentOfFund * 1000, //dollars of 1000
-        shares: percentOfFund * tickerPrice,
+        tickerprices: {
+          symbol,
+          prices: [tickerPriceObj],
+        },
       };
     })
   );
   return completeTickers;
 }
 
+//used on nighgtly update of funds
 async function appendCurrentPrice(arr) {
   const collection = "stock prices";
-
   const completeTickers = Promise.all(
     //always use Promise all when looping
     arr.map(async (element, index) => {
@@ -65,40 +60,75 @@ async function appendCurrentPrice(arr) {
       if (quoteInDb) {
         //if quote in db return the shareprice
         console.log(quoteInDb);
+        const currentPrice = parseFloat(quoteInDb.sharePrice).toFixed(2);
+        const currentPriceObj = {
+          value: currentPrice,
+          timestamp: new Date(),
+        };
+        const hasTickerPrices = element.tickerprices
+          ? element.tickerprices.prices
+          : [];
         return {
           ...element, //return the orginal element with the purchase price added. Will be the and stay the orginal investment of the fund
-          currentPrice: parseFloat(quoteInDb.sharePrice).toFixed(2),
+          currentPrice,
+          tickerprices: {
+            symbol,
+            prices: [...hasTickerPrices, currentPriceObj],
+          },
         };
       }
-      const quote = await getQuote(symbol); //get quote
+      const apiQuote = await getQuote(symbol); //get quote
+      const currentPrice = parseFloat(
+        apiQuote["Global Quote"]["05. price"]
+      ).toFixed(2);
+      console.log(currentPrice);
+      const currentPriceObj = {
+        value: currentPrice,
+        timestamp: new Date(),
+      };
+      const hasTickerPrices = element.tickerprices
+        ? element.tickerprices.prices
+        : [];
       return {
         //if quore not in db, make api call and reutrn the quote object
         ...element, //return the orginal element with the purchase price added. Will be the and stay the orginal investment of the fund
-        currentPrice: parseFloat(quote["Global Quote"]["05. price"]).toFixed(2),
+        currentPrice,
+        tickerprices: {
+          symbol,
+          prices: [...hasTickerPrices, currentPriceObj],
+        },
       };
     })
   );
   return completeTickers;
 }
 
+//used periodically to append daily prices to funds
+async function appendDailyPrice(arr, createDate) {
+  const completeTickers = Promise.all(
+    arr.map(async (element, index) => {
+      const { symbol } = element;
+      const apiDaily = await getDaily(symbol); //get quote
+      const { prices } = apiDaily;
+      const pricesWithinCreateDate = getDatePrices(
+        prices.reverse(),
+        createDate
+      ); //need to reverse so newest to oldest
+      // const hasTickerPrices = element.tickerprices.prices || [];
+      return {
+        //if quore not in db, make api call and reutrn the quote object
+        ...element, //return the orginal element with the purchase price added. Will be the and stay the orginal investment of the fund
+        tickerprices: {
+          symbol,
+          prices: [...pricesWithinCreateDate],
+        },
+      };
+    })
+  );
+  return completeTickers;
+}
+//appending based on start date, pricesinDB need reverse, api prices do not as getIntraDay function reverses it
 async function appendIntraDayPrice({ startDate, arr }) {
-  function getDatePrices(priceArr, dateChecking) {
-    if (!Array.isArray(priceArr)) throw new Error("Must pass an Array");
-    const startDate = new Date(dateChecking);
-    const pricesWithinStartDate = [];
-    const { length } = priceArr;
-    console.log(length);
-    for (let i = 0; i < length; i++) {
-      const dateObject = priceArr[i];
-      const dateOfPrice = new Date(dateObject.timestamp);
-      if (isBefore(startDate, dateOfPrice)) {
-        console.log("Im breaking here", dateObject);
-        break;
-      }
-      pricesWithinStartDate.push(dateObject); //keep loop going, no returns
-    }
-    return [...pricesWithinStartDate].reverse();
-  }
   const collection = "intraday_prices";
   const updatedInvestors = []; //array where the updated joinedinestors will be pushed
   const updatingInvestors = arr.map(async (investor, index) => {
@@ -107,11 +137,8 @@ async function appendIntraDayPrice({ startDate, arr }) {
     const newTickerArr = fundInPlay.tickers.map(async (ticker) => {
       try {
         //search mongo
-        if (!ticker || !ticker.symbol) return; //can delete
         const { symbol } = ticker;
         const priceInDb = await getPriceFromDb(collection, symbol);
-        await priceInDb;
-        // console.log(priceInDb);
         if (priceInDb) {
           const priceObject = getDatePrices(
             priceInDb.prices.reverse(),
@@ -162,6 +189,15 @@ async function appendIntraDayPrice({ startDate, arr }) {
   return updatedInvestors;
 }
 
+async function getCompanyOverView(symbol) {
+  const companyOverviewReq = await axios.get(
+    `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${process.env.Alpha_Key}`
+  );
+  return companyOverviewReq.data;
+}
+
 exports.appendTodaysPrice = appendTodaysPrice;
 exports.appendCurrentPrice = appendCurrentPrice;
 exports.appendIntraDayPrice = appendIntraDayPrice;
+exports.getCompanyOverView = getCompanyOverView;
+exports.appendDailyPrice = appendDailyPrice;
